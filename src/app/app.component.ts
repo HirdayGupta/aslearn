eb443ee102ca64808576c16a33e38b911cfa217c
HirdayGupta <hirday.gupta@gmail.com>
2018-01-20 15:57:15 -0800

    Add auth dependency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(public afAuth: AngularFireAuth) {
+  }
 }
